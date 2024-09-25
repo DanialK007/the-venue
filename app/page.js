f3691fb53f92ea53fb2719cfd440d462ad297a91
DanialK007 <kh2496187@gmail.com>
2024-09-25 10:35:54 +0630

    version 1.0 - Hero import

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import Hero from "@/components/hero";
+import Hero from "@/components/Hero";
 import Menu from "@/components/Menu";
 import Reservation from "@/components/Reservation";
 import Signature from "@/components/Signature";
 import Soul from "@/components/Soul";
-import Image from "next/image";
 
 export default function Home() {
   return (
